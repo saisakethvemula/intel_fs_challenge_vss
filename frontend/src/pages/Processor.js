4721c2a865e7fdb69c3ac442b96be9bbe01c4806
vsaketh <vsaketh@iu.edu>
2024-03-26 19:30:16 -0400

    cards for comparision

--- frontend/src/pages/Processor.js
+++ frontend/src/pages/Processor.js
@@ -3,28 +3,13 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import List from '@mui/material/List';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import { styled } from '@mui/material/styles';
-
-const Demo = styled('div')(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-}));
-
-function generate(element) {
-    return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      }),
-    );
-}
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const Processor = () => {
     const { rowIDs }= useParams();
@@ -59,37 +44,17 @@ const Processor = () => {
     const render_sections = (processor) => {
         const excludedKeys = ['name', 'processor_id'];
         return Object.keys(processor)
-            .filter((key) => !excludedKeys.includes(key))
-            .map((section, index) => (
-            <div key={index}>
-                <Typography variant="h6" component="h3">
-                {section}
-                </Typography>
-                <ul>
+        .filter((key) => !excludedKeys.includes(key))
+        .map((section, index) => (
+            <>
+                <Card.Text as="h5">{section}:</Card.Text>
                 {Object.entries(processor[section]).map(([key, value]) => (
-                    <li key={key}>{key}: {value}</li>
+                    <Card.Text sx={{fontsize: '14px'}} >{key}: {value}</Card.Text>
                 ))}
-                </ul>
-            </div>
-            // <div key={index}>
-            //     <Grid container spacing={2}>
-            //     <Grid item xs={12} md={6}>
-            //     <Demo>
-            //         <List>
-            //         {generate(
-            //             <ListItem>
-            //             <ListItemText
-            //                 primary="Single-line item"
-            //             />
-            //             </ListItem>,
-            //         )}
-            //         </List>
-            //     </Demo>
-            //     </Grid>
-            //     </Grid>
-            // </div>
-            ));
+            </>
+        ));
     };
+    const excludedKeys = ['name', 'processor_id'];
 
     return(
         <>
@@ -103,26 +68,19 @@ const Processor = () => {
             ) : error ? (
                 <div>Error: {error.message}</div>
             ) : (
-                <>
-                <CssBaseline />
-                <Container>
-                <Box sx={{ m: 2 }}>
-                    {processors.map((processor, index) => (
-                        <Card>
-                            <CardContent>
-                                <Typography variant="h5" component="h2">
-                                {processor.name}
-                                </Typography>
-                                <Typography color="textSecondary" gutterBottom>
-                                Processor ID: {processor.processor_id}
-                                </Typography>
-                                {render_sections(processor)}
-                            </CardContent>
-                        </Card>
-                    ))}
-                </Box>
-                </Container>
-                </>
+                <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+                { processors.map((processor, index) => (
+                    <div key={index} style={{ margin: '10px', padding: '10px 10px 10px 10px', border: '1px solid #ccc', borderRadius: '10px', overflow: 'hidden' }}>
+                    <Card>
+                        <Card.Header as="h4">{processor.name}</Card.Header>
+                        <Card.Body>
+                            <Card.Title as="h5">Processor ID: {processor.processor_id}</Card.Title>
+                            {render_sections(processor)}
+                        </Card.Body>
+                    </Card>
+                    </div>
+                ))}
+                </div>
             )}
         </>
     );
